758bc2e195766f33029e0fba763ab170c01dded7
samet <astopaal@gmail.com>
2022-11-10 16:45:35 +0300

    saving question data to db. guid definiton for questions and answers.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,19 +8,28 @@ import { v4 as uuidv4 } from 'uuid';
 const Question = () => {
 
     const [questions, setQuestions] = useState([]);
+    const [questionId, setQuestionId] = useState([String(Date.now())]);
+    const [answerId, setAnswerId] = useState([String(Date.now() + 1)]);
+
 
     const addQuestion = () => {
+
+        setQuestionId([...questionId, String(Date.now())]);
         const newQuestion = {
-            id: questions.length + 1,
-            question: '',
+            id: questionId[questionId.length - 1],
+            question: "",
             answers: [],
         };
         setQuestions([...questions, newQuestion]);
+
+        console.log(questionId)
+
     }
 
     const addAnswer = (questionId) => {
+        setAnswerId([...answerId, String(Date.now())])
         const newAnswer = {
-            id: (String(questionId) + "-" + String(Number(questions[questionId - 1].answers.length) + 1)),
+            id: answerId[answerId.length - 1],
             qid: questionId,
             answer: '',
         };
@@ -55,13 +64,24 @@ const Question = () => {
 
         questionsData = questions;
 
-        questionsData.map((question, index) => {
+        try {
+            let index2 = 0;
+            questionsData.map((question, index) => {
+                console.log("soru idler : " + questionId[index])
+                question.question = document.getElementById(questionId[index]).value;
+                question.answers?.map((answer) => {
+                    console.log("cevap idler : " + answerId)
+                    answer.answer = document.getElementById("answer-" + answerId[index2]).value;
+                    index2++;
+                })
 
-            question.question = document.getElementById(String(index + 1)).value;
-            question.answers?.map((answer, index2) => {
-                answer.answer = document.getElementById("answer-" + String(index + 1) + "-" + String(index2 + 1)).value;
             })
-        })
+
+            console.log(questions)
+        }
+        catch (err){
+            console.log("HATA OLUŞTU. HATA MESAJI : " + err)
+        }
     }
     async function postQuestionData(e) {
         const guid = uuidv4();
@@ -156,21 +176,22 @@ const Question = () => {
                 ))}
             </div>
             <div className='submit-button-area'>
-                <button
-                    className="submit-button"
-                    id="btn-anket-kaydet"
-                    onClick={() => {
-                        saveSurveyForm();
-                    }}
-                >
-                    < IoSaveSharp className='submit-button-icon' />   Anketi Kaydet
-                </button>
+                <form onSubmit={postQuestionData}>
+
+                    <button
+                        type="submit"
+                        className="submit-button"
+                        id="btn-anket-kaydet"
+                        onClick={() => {
+                            saveSurveyForm();
+                        }}
+                    >
+                        < IoSaveSharp className='submit-button-icon' />   Anketi Kaydet
+                    </button>
+                </form>
+
             </div>
-            <form onSubmit={postQuestionData}>
-                <button type="submit">
-                    Backend'e Gönder
-                </button>
-            </form>
+
         </div>
 
     );
