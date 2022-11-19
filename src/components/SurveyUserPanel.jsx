cfa2677df2b45bd616a596863c087cc9c425c5dd
samet <astopaal@gmail.com>
2022-11-19 19:02:26 +0300

    some changes

--- src/components/SurveyUserPanel.jsx
+++ src/components/SurveyUserPanel.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
-import { IoSaveSharp } from "react-icons/io5";
+import React, {useEffect, useState} from 'react'
+import {useSearchParams} from 'react-router-dom'
+import {IoSaveSharp} from "react-icons/io5";
 import './styles/survayuserpanel.css'
 import axios from 'axios'
 
@@ -11,6 +11,7 @@ const SurveyUserPanel = () => {
     const [survey, setSurvey] = useState([]);
     const [answers, setAnswers] = useState([]);
     const [surveyName, setSurveyName] = useState("");
+    const [userInput, setUserInput] = useState([])
 
     const validateSurvey = async () => {
         const response = await axios.post(`http://localhost:8080/get-survey-name`, {
@@ -25,7 +26,6 @@ const SurveyUserPanel = () => {
             })
 
     }
-
 
     const postSurvey = async () => {
         const response = await axios.post(`http://localhost:8080/survey-input`, {
@@ -49,23 +49,33 @@ const SurveyUserPanel = () => {
             })
     }
 
-    const postAnswers = () => {
+    const setChangeToAnswer = (e) => {
 
-    }
+        userInput.map((item) => {
+            if (item?.questionId === e.target.id) {
+                item.answerId = e.target.value
+            } else {
+                userInput.push({
+                    questionId: e.target.id,
+                    answerId: e.target.value
+                })
+            }
+        })
 
-    const saveAnswers = () => {
-
+        // setUserInput( [ ...userInput, {
+        //     questionId :e.target.name,
+        //     answerId : e.target.value
+        // }])
+        console.log(userInput)
     }
 
     useEffect(() => {
-
         validateSurvey();
         fetchAnswers();
         postSurvey();
 
     }, [])
 
-    console.log(survey)
     return (
         <div className='user-survay-container'>
             <div className='survay-title-area'>
@@ -74,42 +84,70 @@ const SurveyUserPanel = () => {
             <div className='survay-questions-container'>
                 <div className="survay-questions-area">
                     {survey.map((soru, index) => (
-                        <div key={index} className="survay-question-and-answer-area">
-                            <div className='survay-question'>
-                                <p>{/* sorunun text: */} {index + 1 + ") "} {soru.TextSoru}</p>
+                            <div key={index} className="survay-question-and-answer-area">
+                                <div className='survay-question'>
+                                    <p>{/* sorunun text: */} {index + 1 + ") "} {soru.TextSoru}</p>
+                                </div>
+                                {answers.map((ans, index) => (
+                                    ans.SoruID === soru.SoruID
+                                        ?
+                                        <div className='survay-answer'>
+                                            <input
+                                                type="radio"
+                                                id={index}
+                                                name={ans.SoruID}
+                                                value={ans.Id}
+                                                onChange={(event) => {
+                                                    let varMi = false;
+                                                    userInput.length <= 0
+                                                        ?
+                                                    setUserInput( [ ...userInput, {
+                                                        info : "sifirdan kucuk",
+                                                        questionId :Number(event.target.name),
+                                                        answerId : Number(event.target.value)
+                                                    }])
+                                                        :
+                                                        userInput.map((el) => {
+                                                            if (el.questionId === Number(event.target.name)) {
+                                                                (el.answerId) = Number(event.target.value)
+                                                                varMi = true;
+                                                            }
+                                                        })
+                                                    if (varMi === false) {
+                                                        setUserInput( [ ...userInput, {
+                                                            info : "sifirdan buyuk",
+                                                            questionId :Number(event.target.name),
+                                                            answerId : Number(event.target.value)
+                                                        }])
+                                                    }
+                                                }}
+                                            />
+                                            <label>{ans.TextCevap}</label>
+                                        </div>
+                                        : <span></span>
+                                ))}
                             </div>
-                            {answers.map((ans, index) => (
-                                ans.SoruID === soru.SoruID
-                                    ?
-                                    <div className='survay-answer'>
-                                        <input type="radio" id={index} name={ans.SoruID} />
-                                        <label>{ans.TextCevap}</label>
-                                    </div>
-
-                                    : <span></span>
-                            ))}
-                        </div>
-                    )
+                        )
                     )}
                 </div>
             </div>
-            <form onSubmit={postAnswers}>
-                <div className='form-submit-button-area'>
-                    <button
-                        type="submit"
-                        className="submit-button"
-                        id="btn-anket-kaydet"
-                        onClick={() => {
-                            saveAnswers();
-                        }}
-                    >
-                        <IoSaveSharp className='submit-button-icon' />   Anketi Kaydet
-                    </button>
-                </div>
-            </form>
+
+            <div className='form-submit-button-area'>
+                <button
+                    type="submit"
+                    className="submit-button"
+                    id="btn-anket-kaydet"
+                    onClick={() => {
+                        console.log((userInput))
+                    }}
+                >
+                    <IoSaveSharp className='submit-button-icon'/> Anketi Kaydet
+                </button>
+            </div>
 
         </div>
     )
+
 }
 
 export default SurveyUserPanel
--- src/components/Surveys.jsx
+++ src/components/Surveys.jsx
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import { styled } from '@mui/material/styles';
 import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import axios from 'axios';
+import QRCode from 'qrcode'
 import './styles/surveys.css'
 
 const Surveys = () => {
 
   const [surveys, setSurveys] = useState([])
+  const [qr, setQr] = useState({})
+
+  const GenerateQRCode = (url, guid) => {
+    QRCode.toDataURL(url, {
+      width: 800,
+      margin: 2,
+      color: {
+        dark: '#335383FF',
+        light: '#EEEEEEFF'
+      }
+    }, (err, url) => {
+      if (err) return console.error(err)
+      setQr({
+        url: url,
+        guid: guid
+      })
+    })
+  }
 
   const fetchSurveys = async () => {
     const response = await axios.get('http://localhost:8080/surveys');
@@ -21,10 +39,10 @@ const Surveys = () => {
 
   return (
     <div>
-      <TableContainer component={Paper}>
+      <TableContainer className="shadow-lg" component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
-            <TableRow>
+            <TableRow className="bg-red-200">
               <TableCell>Anket Adı</TableCell>
               <TableCell align="center" >Cevaplanma Sayısı</TableCell>
               <TableCell align="center" >Durum</TableCell>
@@ -34,14 +52,24 @@ const Surveys = () => {
           </TableHead>
           <TableBody>
             {surveys.map((survey) => (
-              <TableRow key={survey.Guid} component="th" scope="row">
+              <TableRow className="bg-gray-100" key={survey.Guid} component="th" scope="row">
                 <TableCell>{survey.AnketAdi}</TableCell>
                 <TableCell align="center">{survey.AnketiYapanKullaniciSayisi}</TableCell>
                 <TableCell align="center">{(survey.AnketDurumu)}</TableCell>
                 <TableCell align="center">{survey.Guid}</TableCell>
                 <TableCell className="">
-                  <Button className="mr-2" variant="outlined" color="success">DETAY</Button>
-                  <Button className="mr-2" variant="outlined" color="error">PASİF</Button>
+                  <Button className="mr-2 shadow-lg" variant="outlined" color="success">DETAY</Button>
+                  <Button className="mr-2 shadow-lg" variant="outlined" color="error">PASİF</Button>
+                  <Button className="mr-2 shadow-lg" variant="outlined" color="secondary" onClick={() => {
+                    GenerateQRCode(("http://localhost:8080/survey-input?guid=" + survey.Guid), survey.Guid)
+                  }}>QR OLUŞTUR</Button>
+                  {qr.guid === survey.Guid &&
+                    <>
+                      <img className="w-24 h-24" alt="qr" src={qr.url} />
+                      <a href={qr.url} download="qrcode.png">Download</a>
+                    </>
+                  }
+
                 </TableCell>
               </TableRow>
             ))}
